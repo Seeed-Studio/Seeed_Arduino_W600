5422399b26fbebd6378c2c685f78398ebff3e83e
Baozhu Zuo <zuobaozhu@gmail.com>
2020-05-22 08:26:35 +0800

    added: support seeed Seeeduino XIAO

--- examples/log_data/log_data.ino
+++ examples/log_data/log_data.ino
@@ -9,10 +9,11 @@
 #include "w600.h"
 #if defined(HAVE_HWSERIAL1) 
   #define WifiSerial Serial1
-#elif defined(ARDUINO_ARCH_SAMD) 
+#elif defined(ARDUINO_SEEED_ZERO) 
   //the different board of samd have different serialx
   #define WifiSerial Serial2   //serial number of seeeduino_zero (compatible with Wio Lite W600)
-
+#elif defined(SEEED_XIAO_M0) 
+    #define WifiSerial Serial1   
 #else
   SoftwareSerial WifiSerial(2,3);
 #endif
@@ -70,7 +71,7 @@ int socket = -1;
 void setup()
 {
     debug.begin(115200);
-    #if defined(SAMD21)
+    #if defined(ARDUINO_SEEED_ZERO)
         wifi.begin(Serial,9600);
     #else
         wifi.begin(WifiSerial,9600);
